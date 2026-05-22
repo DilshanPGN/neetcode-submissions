21f2285b4e3648a878b1ca80bb8ccf21ed336a4c
Nipun Dilshan <73216265+DilshanPGN@users.noreply.github.com>
2026-05-23 00:59:21 +0530

    Update: duplicate-integer - submission-1

--- Data Structures & Algorithms/duplicate-integer/submission-1.java
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-       HashSet<Integer> hash = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
+
         for (int num : nums) {
-            if (hash.contains(num)) {
+            if (!set.add(num)) {
                 return true;
-            } else {
-                hash.add(num);
             }
         }
-        return false; 
+
+        return false;
     }
 }
